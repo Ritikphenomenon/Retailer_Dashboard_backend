cf801481e009413a2404005d94dd564f98b49a63
Ritikphenomenon <ritikphenomenon@gmail.com>
2024-01-14 22:42:53 +0530

    first commit

---
+++ db/index.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+// Define mongoose schemas
+
+const userSchema = new mongoose.Schema({
+    username: { type: String },
+    password: String,
+  });
+
+  
+
+  const ProductSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String,
+    published: Boolean,
+    Id:String
+    });
+
+      
+  // Define mongoose models
+  const User = mongoose.model('User', userSchema);
+  const Product= mongoose.model('Product', ProductSchema);
+  
+   
+  module.exports = {
+    User,
+    Product
+  }
---
+++ routes/user.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const { authenticateJwt, SECRET } = require("../middleware/index");
+const { User, Product } = require("../db");
+const jwt=require('jsonwebtoken')
+const router = express.Router();
+
+
+ 
+
+router.post('/signup', async (req, res) => {
+    try {
+      const { username, password,repassword } = req.body;
+      if(password!=repassword){
+        return res.json({message:'Retype password does not match'});
+      }
+      const user = await User.findOne({ username });
+  
+      if (user) {
+        
+        return res.json({ message: 'User already exists' });
+      }
+  
+      const newUser = new User({ username, password });
+      await newUser.save();
+      const token = jwt.sign({ username, role: 'user' }, SECRET, { expiresIn: '1h' });
+      res.json({ message: 'User created successfully', token });
+    } catch (error) {
+      console.error('Error during user signup:', error.message);
+      res.status(500).json({ message: 'Server error during user signup' });
+    }
+  });
+  
+  router.post('/login', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username, password });
+  
+      if (user) {
+        const token = jwt.sign({ username, role: 'user' }, SECRET, { expiresIn: '1h' });
+        res.json({ message: 'Logged in successfully', token });
+      } else {
+        res.status(403).json({ message: 'Invalid username or password' });
+      }
+    } catch (error) {
+      console.error('Error during user login:', error.message);
+      res.status(500).json({ message: 'Server error during user login' });
+    }
+  });
+  
+
+  
+module.exports = router

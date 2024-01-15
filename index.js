f743868f0b97a83097b77aec33fb5ef9ce69a899
Ritik Raj Pandey <ritikphenomenon@gmail.com>
2024-01-15 20:47:51 +0530

    cors issue to check

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const userRouter=require("./routes/user");
 
 
 const app=express();
-app.use(cors());
+app.use(cors({ origin: 'https://retailer-dashboard-frontend.vercel.app' }));
 app.use(express.json());
 app.use("/users",userRouter);
 
